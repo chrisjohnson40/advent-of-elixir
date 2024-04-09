817ba38c406c3c98b48d455a06613aadde28c53f
mtallentb <matt.brown@fortyau.com>
2024-04-08 21:47:50 -0500

    empties whitelist

--- config/config.exs
+++ config/config.exs
@@ -1,25 +1,28 @@
 import Config
 
 # somewhere in your config file
-config :git_hooks,
-  auto_install: true,
-  verbose: true,
-  branches: [
-    whitelist: ["feature/.*", "bugfix/.*"],
-    blacklist: ["main"]
-  ],
-  hooks: [
-    pre_commit: [
-      tasks: [
-        {:cmd, "mix format --check-formatted"}
-      ]
+if Mix.env() == :dev do
+  IO.puts("Loading config file: #{__ENV__.file}")
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    branches: [
+      whitelist: [],
+      blacklist: ["main"]
     ],
-    pre_push: [
-      verbose: false,
-      tasks: [
-        {:cmd, "mix credo"},
-        {:cmd, "mix test --color"},
-        {:cmd, "echo 'success!'"}
+    hooks: [
+      pre_commit: [
+        tasks: [
+          {:cmd, "mix format --check-formatted"}
+        ]
+      ],
+      pre_push: [
+        verbose: false,
+        tasks: [
+          {:cmd, "mix credo"},
+          {:cmd, "mix test --color"},
+          {:cmd, "echo 'success!'"}
+        ]
       ]
     ]
-  ]
+end
