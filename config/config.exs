bbce611ba823f9c1c47ce548a90184430c1f886b
mtallentb <matt.brown@fortyau.com>
2024-04-08 21:21:00 -0500

    adds git-hooks package w/ config

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+import Config
+
+# somewhere in your config file
+if Mix.env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    branches: [
+      whitelist: ["feature/.*"],
+      blacklist: ["main"]
+    ],
+    hooks: [
+      pre_commit: [
+        tasks: [
+          {:cmd, "mix format --check-formatted"}
+        ]
+      ],
+      pre_push: [
+        verbose: false,
+        tasks: [
+          {:cmd, "mix dialyzer"},
+          {:cmd, "mix test --color"},
+          {:cmd, "echo 'success!'"}
+        ]
+      ]
+    ]
+end
