aa7ef03f3d9c66dbe5c845f4f25b996317ff7890
mtallentb <matt.brown@fortyau.com>
2024-04-08 22:29:28 -0500

    resolves git hooks config

--- config/config.exs
+++ config/config.exs
@@ -2,27 +2,29 @@ import Config
 
 # somewhere in your config file
 if Mix.env() == :dev do
-  IO.puts("Loading config file: #{__ENV__.file}")
-  config :git_hooks,
-    auto_install: true,
-    verbose: true,
-    branches: [
-      whitelist: [],
-      blacklist: ["main"]
+config :advent_of_elixir, :git_hooks,
+  auto_install: true,
+  verbose: true,
+  branches: [
+    whitelist: ["feature/.*"],
+    blacklist: ["main"]
+  ],
+  hooks: [
+    pre_commit: [
+      tasks: [
+        {:cmd, "mix format --check-formatted"}
+      ]
     ],
-    hooks: [
-      pre_commit: [
-        tasks: [
-          {:cmd, "mix format --check-formatted"}
-        ]
-      ],
-      pre_push: [
-        verbose: false,
-        tasks: [
-          {:cmd, "mix credo"},
-          {:cmd, "mix test --color"},
-          {:cmd, "echo 'success!'"}
-        ]
+    pre_push: [
+      verbose: false,
+      tasks: [
+        {:cmd, "mix credo"},
+        {:cmd, "mix test --color"},
+        {:cmd, "echo 'success!'"}
       ]
     ]
+  ]
+
 end
+
+import_config "#{config_env()}.exs"
