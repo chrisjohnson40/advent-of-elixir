c388020d719d204431ca9327209468db73c8ad61
mtallentb <matt.brown@fortyau.com>
2024-04-08 21:33:21 -0500

    removes :dev env check for :git_hooks config

--- config/config.exs
+++ config/config.exs
@@ -1,27 +1,25 @@
 import Config
 
 # somewhere in your config file
-if Mix.env() == :dev do
-  config :git_hooks,
-    auto_install: true,
-    verbose: true,
-    branches: [
-      whitelist: ["feature/.*", "bugfix/.*"],
-      blacklist: ["main"]
+config :git_hooks,
+  auto_install: true,
+  verbose: true,
+  branches: [
+    whitelist: ["feature/.*", "bugfix/.*"],
+    blacklist: ["main"]
+  ],
+  hooks: [
+    pre_commit: [
+      tasks: [
+        {:cmd, "mix format --check-formatted"}
+      ]
     ],
-    hooks: [
-      pre_commit: [
-        tasks: [
-          {:cmd, "mix format --check-formatted"}
-        ]
-      ],
-      pre_push: [
-        verbose: false,
-        tasks: [
-          {:cmd, "mix credo"},
-          {:cmd, "mix test --color"},
-          {:cmd, "echo 'success!'"}
-        ]
+    pre_push: [
+      verbose: false,
+      tasks: [
+        {:cmd, "mix credo"},
+        {:cmd, "mix test --color"},
+        {:cmd, "echo 'success!'"}
       ]
     ]
-end
+  ]
